b2610823cdf275efc24a700aa743ae682a812589
LemmoTresto <maxberkelmans@live.nl>
2017-05-26 14:50:05 +0200

    Add files via upload

---
+++ src/me/max/simplefirstgame/Handler.java
@@ -0,0 +1,35 @@
+package me.max.simplefirstgame;
+
+import java.awt.*;
+import java.util.LinkedList;
+
+/**
+ * Created by max on 25-5-2017.
+ */
+public class Handler {
+
+    LinkedList<GameObject> object = new LinkedList<GameObject>();
+
+    public void tick(){
+        for (int i = 0; i < object.size(); i++){
+            GameObject tempObject = object.get(i);
+            tempObject.tick();
+        }
+    }
+
+    public void render(Graphics g){
+        for (int i = 0; i < object.size(); i++) {
+            GameObject tempObject = object.get(i);
+            tempObject.render(g);
+        }
+    }
+
+    public void addObject(GameObject object){
+        this.object.add(object);
+    }
+
+    public void removeObject(GameObject object){
+        this.object.remove(object);
+    }
+
+}
---
+++ src/me/max/simplefirstgame/ID.java
@@ -0,0 +1,12 @@
+package me.max.simplefirstgame;
+
+/**
+ * Created by max on 25-5-2017.
+ */
+public enum ID {
+
+    Player(),
+    BasicEnemy(),
+    Trial();
+
+}
